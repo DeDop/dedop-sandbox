f5237b5532269364e08ab42ca12e18d3da2db0ed
Norman Fomferra <norman.fomferra@gmail.com>
2015-12-15 22:22:53 +0100

    updates

--- README.md
+++ README.md
@@ -7,33 +7,50 @@ Contains DeDop Experiments and Prototypes:
 * ``frontend`` -  Hosts DeDop GUI ``dedopgui`` which is implemented as an **electron** Desktop application and depends on 
   a running WebService ``dedopws`` on localhost.
 
-## Setup Development
+## backend
 
-### backend
+Build and run
 
-The backend hosts the DeDeop  implemented 
-Install Python 3.4. Then
+1. Install Python 3.4.
+2. Install required libs
 
-	> cd backend
     > pip install falcon
     > pip install CherryPy
+
+3. Run
+
+	> cd backend
 	> python setup.py develop
 	> dedopws
 
-### frontend
+Read more:
 
-Install ``node.js`` to get the node package manager ``npm``. Then
-
-	> cd frontend
-	> npm install electron-prebuilt -g
-	> electron .
-
-## Read more
-
-* WSGI, PEP 3333  
+* WSGI, PEP 3333
 * Falcon is a ridiculously fast, minimalist Python WSGI web framework for building cloud APIs and app backends.
   See http://falconframework.org/
 * CherryPy, a reliable, HTTP/1.1-compliant, WSGI thread-pooled webserver. It's 100% Python, and can even be used by Jython.
   See http://www.cherrypy.org/
 * Gunicorn, another popular Python Web Framework. Heavily relies on Unix, no Windows support as of version 19.
   See http://gunicorn.org/
+
+### frontend
+
+Build and run
+
+1. Install Node.js to get node's package manager ``npm``.
+2. Globally install electron
+
+	> npm install electron-prebuilt -g
+
+3. Run
+
+	> cd frontend
+	> electron .
+
+Read more:
+
+* electron, build cross platform desktop apps with web technologies.
+  See http://electron.atom.io/
+* Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.
+  See https://nodejs.org/en/
+* https://www.chromium.org/, https://en.wikipedia.org/wiki/Chromium_%28web_browser%29
--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,8 @@
 'use strict';
 const electron = require('electron');
 const app = electron.app;  // Module to control application life.
+const Menu = electron.Menu;
+const Tray = electron.Tray;
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
 
 // Report crashes to our server.
@@ -8,7 +10,8 @@ electron.crashReporter.start();
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+var mainWindow = null;
+var appIcon = null;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
@@ -22,14 +25,35 @@ app.on('window-all-closed', function() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
+
+  appIcon = new Tray('images/dedop.png');
+  var contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', type: 'radio' },
+    { label: 'Item2', type: 'radio' },
+    { label: 'Item3', type: 'radio', checked: true },
+    { label: 'Item4', type: 'radio' }
+  ]);
+  appIcon.setToolTip('This is my application.');
+  appIcon.setContextMenu(contextMenu);
+
+
   // Create the browser window.
-  mainWindow = new BrowserWindow({title: 'DeDop', width: 800, height: 600});
+  mainWindow = new BrowserWindow({
+    title: 'DeDop',
+    icon: 'images/dedop.png',
+    width: 800, height: 600
+  });
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
+
+  // Emitted when the window leaves full screen state.
+  mainWindow.on('leave-full-screen', function() {
+    appIcon.displayBalloon({title: 'Hey, hey!', content: 'You\'ve left the cool full-screen mode.'});
+  });
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
@@ -37,5 +61,6 @@ app.on('ready', function() {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
+    appIcon = null;
   });
 });
