e0fc5b34aff644825ee8abe7c764dd2cdab5c5b3
Norman Fomferra <norman.fomferra@gmail.com>
2015-12-16 22:54:31 +0100

    decided to use w2ui plus jQuery for the prototype

--- frontend/main.js
+++ frontend/main.js
@@ -14,53 +14,53 @@ var mainWindow = null;
 var appIcon = null;
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+app.on('window-all-closed', function () {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function() {
+app.on('ready', function () {
 
-  appIcon = new Tray('images/dedop.png');
-  var contextMenu = Menu.buildFromTemplate([
-    { label: 'Item1', type: 'radio' },
-    { label: 'Item2', type: 'radio' },
-    { label: 'Item3', type: 'radio', checked: true },
-    { label: 'Item4', type: 'radio' }
-  ]);
-  appIcon.setToolTip('This is my application.');
-  appIcon.setContextMenu(contextMenu);
+    appIcon = new Tray('images/dedop.png');
+    var contextMenu = Menu.buildFromTemplate([
+        {label: 'Item1', type: 'radio'},
+        {label: 'Item2', type: 'radio'},
+        {label: 'Item3', type: 'radio', checked: true},
+        {label: 'Item4', type: 'radio'}
+    ]);
+    appIcon.setToolTip('This is my application.');
+    appIcon.setContextMenu(contextMenu);
 
 
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    title: 'DeDop',
-    icon: 'images/dedop.png',
-    width: 800, height: 600
-  });
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        title: 'DeDop',
+        icon: 'images/dedop.png',
+        width: 800, height: 600
+    });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+    // and load the index.html of the app.
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  // Open the DevTools.
-  //mainWindow.webContents.openDevTools();
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
 
-  // Emitted when the window leaves full screen state.
-  mainWindow.on('leave-full-screen', function() {
-    appIcon.displayBalloon({title: 'Hey, hey!', content: 'You\'ve left the cool full-screen mode.'});
-  });
+    // Emitted when the window leaves full screen state.
+    mainWindow.on('leave-full-screen', function () {
+        appIcon.displayBalloon({title: 'Hey, hey!', content: 'You\'ve left the cool full-screen mode.'});
+    });
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-    appIcon = null;
-  });
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+        appIcon = null;
+    });
 });
